75572c0f0ff31d082ef2c40c4f8eb3c16d6a7ab5
priyanshshah2442 <108476017+priyanshshah2442@users.noreply.github.com>
2023-04-08 12:54:02 +0530

    Add files via upload

---
+++ DbContextDemo.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Methods.Models;
+public class DbContextDemo:DbContext
+{
+   
+        public DbContextDemo(DbContextOptions<DbContextDemo> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Student> Student { get; set; }
+
+    }
+	
+
